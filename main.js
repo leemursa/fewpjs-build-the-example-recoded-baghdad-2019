ee8f30263ab428d224ccfc855ca61f96f57b1e98
Mursaleen Taqi <leemursa@gmail.com>
2020-01-22 15:23:54 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
 
+// Your JavaScript code goes here!
+
+
+
+//------------------------------------------------------------------------------
+// Ignore after this point. Used only for demo purposes
+//------------------------------------------------------------------------------
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
